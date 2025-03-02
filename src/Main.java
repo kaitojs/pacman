1d63d5a34458ac6808ed3fac850ad6f01b6ae963
bytesculptors <tuannghia200603@gmail.com>
2025-03-02 09:57:27 +0700

    init project

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        int rowCount = 21;
+        int colCount = 19;
+        int titleSize = 32;
+        int boardWidth = colCount * titleSize;
+        int boardHeight = rowCount * titleSize;
+
+        JFrame frame = new JFrame("Pac Man");
+        frame.setVisible(true);
+        frame.setSize(boardWidth, boardHeight);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
